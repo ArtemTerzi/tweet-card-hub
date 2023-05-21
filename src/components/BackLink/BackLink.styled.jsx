f046783dfd23eb9befaa033fec9d7e33e34d233e
Artem Terzi <terzi.artem.yuriyovych@gmail.com>
2023-05-21 10:37:30 +0300

    CHANGE: Changed repo name

--- src/components/BackLink/BackLink.styled.jsx
+++ src/components/BackLink/BackLink.styled.jsx
@@ -5,6 +5,7 @@ export const Button = styled.button`
   font-size: 18px;
   line-height: 22px;
   padding: 8px 24px;
+  color: #471ca9;
 
   background: #ebd8ff;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/tweets-card">
+      <BrowserRouter basename="/tweet-card-hub">
         <App />
       </BrowserRouter>
     </Provider>
